191b523e8c311de1fdf04843b7efb198acb0ba85
jscd <>
2019-01-22 14:27:32 -0500

    Add transmission class

--- src/main/include/Transmission.h
+++ src/main/include/Transmission.h
@@ -9,5 +9,37 @@ enum Direction {
 	Reverse
 };
 
-template<typename Motor_Controller> 
-Motor_Controller& make_transmission(std::initializer_list<std::pair<int,Direction>> motor_info);
+template <typename Motor_Controller>
+class Transmission {
+public:
+	Transmission(Transmission const& t) = delete;
+	Transmission& operator=(Transmission const& t) = delete;
+
+	Transmission(std::initializer_list<std::pair<int, Direction>> motor_info) : 
+			leader( (motor_info.end()-1)->first ) {
+		auto leader_info = *(motor_info.end()-1);
+		leader.SetInverted(leader_info.second);
+
+		followers.reserve(motor_info.size()-1);
+		for(auto m_inf = motor_info.begin(); m_inf != motor_info.end()-1; m_inf++)
+			followers.push_back( (new Motor_Controller( m_inf->first) ) );
+
+		for(int i = 0; i < motor_info.size()-1; i++){
+			followers[i]->SetInverted((motor_info.begin()+i)->second);
+			followers[i]->Set(ControlMode::Follower, leader_info.first);
+		}
+	}
+
+	~Transmission(){
+		for(Motor_Controller* follower : followers)
+			follower->~Motor_Controller();
+	}
+
+	void Set(ControlMode control_mode, double value){
+		leader.Set(control_mode, value);
+	}
+
+private:
+	Motor_Controller leader;
+	std::vector<Motor_Controller*> followers;
+};
