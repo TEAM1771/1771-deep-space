0d58e21e3dcaad582778712f81a0237f0af0edc2
jscd <>
2019-01-24 19:24:25 -0500

    Add comments to Transmission class

--- src/main/include/Transmission.h
+++ src/main/include/Transmission.h
@@ -12,29 +12,36 @@ enum Direction {
 template <typename Motor_Controller>
 class Transmission {
 public:
+	// Don't allow copy/move constructors
 	Transmission(Transmission const& t) = delete;
 	Transmission& operator=(Transmission const& t) = delete;
 
+	// Constructor takes an initializer list of int-direction pairs
 	Transmission(std::initializer_list<std::pair<int, Direction>> motor_info) : 
 			leader( (motor_info.end()-1)->first ) {
-		auto leader_info = *(motor_info.end()-1);
+		// Initialize leader motor and create a const variable to store the leader motor's info
+		auto const leader_info = *(motor_info.end()-1);
 		leader.SetInverted(leader_info.second);
 
+		// Store follower motors in vector
 		followers.reserve(motor_info.size()-1);
 		for(auto m_inf = motor_info.begin(); m_inf != motor_info.end()-1; m_inf++)
 			followers.push_back( (new Motor_Controller( m_inf->first) ) );
 
+		// Invert followers depending on direction and set to follow mode
 		for(int i = 0; i < motor_info.size()-1; i++){
 			followers[i]->SetInverted((motor_info.begin()+i)->second);
 			followers[i]->Set(ControlMode::Follower, leader_info.first);
 		}
 	}
 
+	// Desructor
 	~Transmission(){
 		for(Motor_Controller* follower : followers)
 			follower->~Motor_Controller();
 	}
 
+	// Set leader motor
 	void Set(ControlMode control_mode, double value){
 		leader.Set(control_mode, value);
 	}
