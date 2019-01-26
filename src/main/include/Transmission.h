c876a03e152637e0008b0edff59b20a9a894ca01
jscd <desimpliciis@gmail.com>
2019-01-26 16:01:12 -0500

    Fix seg faults in Transmission class

--- src/main/include/Transmission.h
+++ src/main/include/Transmission.h
@@ -15,20 +15,17 @@ public:
 	Transmission(std::initializer_list<std::pair<int, Direction>> motor_info) : 
 			leader( (motor_info.begin())->first ) {
 		// Initialize leader motor and create a const variable to store the leader motor's info
-		auto const leader_num = (motor_info.begin())->first;
+		int const leader_num = (motor_info.begin())->first;
 		leader.SetInverted((motor_info.begin())->second);
 
 		// Store follower motors in string
-
 		followers.reserve(motor_info.size()-1);
-		for(auto m_inf = motor_info.begin(); m_inf != motor_info.end()-1; m_inf++)
-			followers +=  (new Motor_Controller( m_inf->first) );
+		for(auto m_inf = motor_info.begin()+1; m_inf != motor_info.end(); m_inf++)
+			followers +=  (new Motor_Controller(m_inf->first) );
 
 		// Invert followers depending on direction and set to follow mode
-		int i = -2;
-		for(auto const& m_info : motor_info){
-			if(++i==-1) continue;		// Skip the leader, and incriment i
-			followers[i]->SetInverted(m_info.second);
+		for(int i = 0; i < motor_info.size()-1; i++){
+			followers[i]->SetInverted((motor_info.begin()+i)->second);
 			followers[i]->Set(ControlMode::Follower, leader_num);
 		}
 	}
