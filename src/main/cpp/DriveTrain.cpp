80a0f406ee49925217e1306dd11e9e0b22e5bf4f
jscd <desimpliciis@gmail.com>
2019-02-15 10:49:01 -0500

    Fix avgVelocity value to use velocity instead of position and reorganize if statements

--- src/main/cpp/DriveTrain.cpp
+++ src/main/cpp/DriveTrain.cpp
@@ -8,10 +8,10 @@ void DriveTrain::tank(double lrate, double rrate) {
     ltrm->Set(ControlMode::PercentOutput, lrate);
     rtrm->Set(ControlMode::PercentOutput, rrate);
 
-    double avgVelocity = (ltrm->GetSelectedSensorPosition()+rtrm->GetSelectedSensorPosition())/2.0;
-    if(avgVelocity >= DRIVETRAIN::SOLENOID::SHIFT_UP_SPEED && shift_status == DRIVETRAIN::SOLENOID::SHIFT_DOWN)
+    double avgVelocity = (ltrm->GetSelectedSensorVelocity()+rtrm->GetSelectedSensorVelocity())/2.0;
+    if(shift_status == DRIVETRAIN::SOLENOID::SHIFT_DOWN && avgVelocity >= DRIVETRAIN::SOLENOID::SHIFT_UP_SPEED)
         shift(DRIVETRAIN::SOLENOID::SHIFT_UP);
-    else if(avgVelocity <= DRIVETRAIN::SOLENOID::SHIFT_DOWN_SPEED && shift_status == DRIVETRAIN::SOLENOID::SHIFT_UP)
+    else if(shift_status == DRIVETRAIN::SOLENOID::SHIFT_UP && avgVelocity <= DRIVETRAIN::SOLENOID::SHIFT_DOWN_SPEED)
         shift(DRIVETRAIN::SOLENOID::SHIFT_DOWN);
 }
 
