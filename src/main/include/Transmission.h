22563412b6d62b0c9a378c358806f90352339569
jscd <desimpliciis@gmail.com>
2019-02-18 15:48:14 -0500

    Make Transmission class use first motor as leader instead of last motor

--- src/main/include/Transmission.h
+++ src/main/include/Transmission.h
@@ -13,19 +13,19 @@ class Transmission {
 public:
 
 	Transmission(std::initializer_list<std::pair<int, Direction>> motor_info) : 
-			leader( (motor_info.end()-1)->first ) {
+			leader( (motor_info.begin())->first ) {
 		// Initialize leader motor and create a const variable to store the leader motor's info
-		auto const leader_info = *(motor_info.end()-1);
+		auto const leader_info = *(motor_info.begin());
 		leader.SetInverted(leader_info.second);
 
 		// Store follower motors in vector
 		followers.reserve(motor_info.size()-1);
-		for(auto m_inf = motor_info.begin(); m_inf != motor_info.end()-1; m_inf++)
+		for(auto m_inf = motor_info.begin()+1; m_inf != motor_info.end(); m_inf++)
 			followers +=  (new Motor_Controller( m_inf->first) );
 
 		// Invert followers depending on direction and set to follow mode
 		for(int i = 0; i < motor_info.size()-1; i++){
-			followers[i]->SetInverted((motor_info.begin()+i)->second);
+			followers[i]->SetInverted((motor_info.begin()+i+1)->second);
 			followers[i]->Set(ControlMode::Follower, leader_info.first);
 		}
 	}
