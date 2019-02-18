d714fb15b9dd2a40b8da07d17229c6cf1aadf02d
jscd <desimpliciis@gmail.com>
2019-02-18 15:44:59 -0500

    Add Jacks code

---
+++ src/main/cpp/Jacks.cpp
@@ -0,0 +1,41 @@
+#include <Jacks.h>
+
+Jacks::Jacks() {}
+
+void Jacks::init() {
+    front_left.ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, 30);
+    front_right.ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, 30);
+    back.ConfigSelectedFeedbackSensor(FeedbackDevice::QuadEncoder, 0, 30);
+
+    front_left.SetSensorPhase(true);
+    front_right.SetSensorPhase(true);
+    back.SetSensorPhase(true);
+
+    front_left.Config_kP(0, JACKS::P, 30);
+    front_left.Config_kI(0, JACKS::I, 30);
+    front_left.Config_kD(0, JACKS::D, 30);
+
+    front_right.Config_kP(0, JACKS::P, 30);
+    front_right.Config_kI(0, JACKS::I, 30);
+    front_right.Config_kD(0, JACKS::D, 30);
+
+    back.Config_kP(0, JACKS::P, 30);
+    back.Config_kI(0, JACKS::I, 30);
+    back.Config_kD(0, JACKS::D, 30);
+}
+
+void Jacks::raise() {
+    front_left.Set(ControlMode::PercentOutput, JACKS::POSITION::RAISED);
+    front_right.Set(ControlMode::PercentOutput, JACKS::POSITION::RAISED);
+    back.Set(ControlMode::PercentOutput, JACKS::POSITION::RAISED);
+}
+
+void Jacks::lower() {
+    front_left.Set(ControlMode::PercentOutput, JACKS::POSITION::LOWERED);
+    front_right.Set(ControlMode::PercentOutput, JACKS::POSITION::LOWERED);
+    back.Set(ControlMode::PercentOutput, JACKS::POSITION::LOWERED);
+}
+
+void Jacks::drive(double rate) {
+    driver.Set(ControlMode::PercentOutput, rate);
+}
---
+++ src/main/include/Jacks.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <ctre/Phoenix.h>
+
+#include <Constants.h>
+
+class Jacks {
+public:
+    Jacks();
+    
+    void init();
+
+    void raise();
+    void lower();
+    void drive(double rate);
+
+private:
+    WPI_TalonSRX front_left{JACKS::FRONT_LEFT_PORT}, front_right{JACKS::FRONT_RIGHT_PORT}, back{JACKS::BACK_PORT};
+    WPI_TalonSRX driver{JACKS::DRIVER_PORT};
+};
