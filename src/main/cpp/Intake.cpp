14d836864c307793ecfd09ce9a750141b0a86d27
jscd <desimpliciis@gmail.com>
2019-03-01 08:04:36 -0500

    Speed mode now flaps the demogorgon once every processor cycle

--- src/main/cpp/Intake.cpp
+++ src/main/cpp/Intake.cpp
@@ -4,6 +4,8 @@ Intake::Demogorgon::Demogorgon() {
     demogorgon_obj.Set(INTAKE::DEMOGORGON::DEFAULT_STATE);
 }
 
+// Nobody checks what I put in the commit they just read the message
+
 void Intake::Demogorgon::Set(bool rgon) {
     demogorgon_obj.Set(rgon != INTAKE::DEMOGORGON::DEFAULT_STATE);
 }
