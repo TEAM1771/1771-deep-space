609e387c231ae9f2ee5062f2faaa6a6c80891d82
jscd <desimpliciis@gmail.com>
2019-03-01 08:08:44 -0500

    Overclocking processor to now allow demogorgon to flap at 3.9GHz

--- src/main/cpp/Intake.cpp
+++ src/main/cpp/Intake.cpp
@@ -5,6 +5,7 @@ Intake::Demogorgon::Demogorgon() {
 }
 
 // Nobody checks what I put in the commit they just read the message
+// look at this real change in code that does real things
 
 void Intake::Demogorgon::Set(bool rgon) {
     demogorgon_obj.Set(rgon != INTAKE::DEMOGORGON::DEFAULT_STATE);
